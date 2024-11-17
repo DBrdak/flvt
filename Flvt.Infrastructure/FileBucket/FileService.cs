f95727030ff2878ae500d6c2fd05e8a7aaec440d
DBrdak <brdak02dominik@gmail.com>
2024-11-17 02:40:43 +0100

    s3

--- Flvt.Infrastructure/FileBucket/FileService.cs
+++ Flvt.Infrastructure/FileBucket/FileService.cs
@@ -33,6 +33,7 @@ internal sealed class FileService : IFileService
                 }
             });
 
+
         var filePath = $"ads/{filter.Id}.json";
 
         var response = await _s3Bucket.WriteFileAsync(
